af7d4d8a2dee4c425de021c0d4879b3ebea4707d
Jakub Uher <uhel@Jakubs-MacBook-Pro-2.local>
2022-09-27 21:04:34 +0100

    Update: Hero image

--- pages/index.js
+++ pages/index.js
@@ -3,7 +3,7 @@ import {useEffect, useState} from "react";
 import Header from "../components/Header/Header";
 import PlayerList from "../components/PlayerList/PlayerList";
 import NewPlayer from "../components/NewPlayer/NewPlayer";
-import heroUrl from '../public/assets/hero2.png';
+import heroUrl from '../public/assets/hero3.png';
 import favUrl from '../public/fav.svg';
 import hygraphClient from "../lib/hygraphClient";
 import {gql} from "graphql-request";
