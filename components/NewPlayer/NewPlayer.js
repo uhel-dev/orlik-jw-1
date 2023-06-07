ec9c1e0d227096b4d59213e2e62ceb9bcdf55c36
JakubUherDev <jjuherdev@gmail.com>
2023-06-07 10:24:01 +0100

    comment out table

--- components/NewPlayer/NewPlayer.js
+++ components/NewPlayer/NewPlayer.js
@@ -29,21 +29,21 @@ function NewPlayer(props) {
                     </h2>
                 </div>
 
-                <form className="flex flex-col" onSubmit={handleSubmit}>
-                        <div className={'mx-auto space-x-4 p-5 flex flex-row'}>
-                            <div className="">
-                                <label htmlFor="exampleFormControlInput1" className="form-label inline-block mb-2 text-gray-700">Imie i Nazwisko</label>
-                                <input onMouseDown={handleListRefresh} id="exampleFormControlInput1" className={'placeholder-black text-black p-4 ml-4'} type="text" placeholder="Wpisz Imie i Nazwisko" autoComplete="off" required/>
-                            </div>
-                        </div>
-                        <div className={'mx-auto space-x-4 p-5 flex flex-row'}>
-                            <div className="">
-                                <label htmlFor="exampleFormControlInput2" className="form-label inline-block mb-2 text-gray-700">Numer Telefonu</label>
-                                <input id="exampleFormControlInput2" className={'placeholder-black text-black p-4 ml-4'} type="number" placeholder="Wpisz Numer Telefonu" autoComplete="off" required/>
-                            </div>
-                        </div>
-                    <button className={'m-8 bg-[#32d7d3] w-1/3 md:w-1/6 p-2 mx-auto text-white font-bold'} type="submit"><span>Zapisz Sie</span></button>
-                </form>
+                {/*<form className="flex flex-col" onSubmit={handleSubmit}>*/}
+                {/*        <div className={'mx-auto space-x-4 p-5 flex flex-row'}>*/}
+                {/*            <div className="">*/}
+                {/*                <label htmlFor="exampleFormControlInput1" className="form-label inline-block mb-2 text-gray-700">Imie i Nazwisko</label>*/}
+                {/*                <input onMouseDown={handleListRefresh} id="exampleFormControlInput1" className={'placeholder-black text-black p-4 ml-4'} type="text" placeholder="Wpisz Imie i Nazwisko" autoComplete="off" required/>*/}
+                {/*            </div>*/}
+                {/*        </div>*/}
+                {/*        <div className={'mx-auto space-x-4 p-5 flex flex-row'}>*/}
+                {/*            <div className="">*/}
+                {/*                <label htmlFor="exampleFormControlInput2" className="form-label inline-block mb-2 text-gray-700">Numer Telefonu</label>*/}
+                {/*                <input id="exampleFormControlInput2" className={'placeholder-black text-black p-4 ml-4'} type="number" placeholder="Wpisz Numer Telefonu" autoComplete="off" required/>*/}
+                {/*            </div>*/}
+                {/*        </div>*/}
+                {/*    <button className={'m-8 bg-[#32d7d3] w-1/3 md:w-1/6 p-2 mx-auto text-white font-bold'} type="submit"><span>Zapisz Sie</span></button>*/}
+                {/*</form>*/}
             </div>
         </div>
     )
